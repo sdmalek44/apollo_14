079802937c1490fe7a47b19385fe88f947d76318
Dione <dionew1@gmail.com>
2018-07-09 16:45:54 -0600

    Initial structure

--- db/migrate/20180706181538_astronaut_space_missions.rb
+++ db/migrate/20180706181538_astronaut_space_missions.rb
@@ -1,4 +1,7 @@
 class AstronautSpaceMissions < ActiveRecord::Migration[5.1]
   def change
+    create_table :astronaut_space_missions do |t|
+      t.references :astronaut, foreign_key: true
+      t.references :space_mission, foreign_key: true
   end
 end
